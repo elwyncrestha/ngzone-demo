6c5c917169a626bd9ec9fee7e43b9099a80859c4
Elvin Shrestha <elwyncrestha@gmail.com>
2020-04-26 21:35:47 +0545

    Added zone in angular example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngzone-demo';
+
+  progress = 0;
+
+  constructor(private zone: NgZone) {}
+
+  processWithinAngularZone() {
+    this.progress = 0;
+    // this.increaseProgress(() => console.log('Done!'));
+    this.processOutsideAngularZone();
+  }
+
+  increaseProgress(doneCallback: () => void) {
+    this.progress += 1;
+    console.log(`Current progress: ${this.progress}%`);
+
+    if (this.progress < 100) {
+      window.setTimeout(() => {
+        this.increaseProgress(doneCallback);
+      }, 10);
+    } else {
+      doneCallback();
+    }
+  }
+
+  processOutsideAngularZone() {
+    this.progress = 0;
+    this.zone.runOutsideAngular(() => {
+      this.increaseProgress(() => {
+        this.zone.run(() => {
+          console.log('Outside Done!');
+        });
+      });
+    });
+  }
 }
